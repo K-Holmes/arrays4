de2d8a7fa1ecdbf86769f8ae3f7174b7d1921428
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop.com>
2022-10-20 18:57:40 -0400

    "Added user readability"

--- src/ArraysPractice.java
+++ src/ArraysPractice.java
@@ -1,9 +1,10 @@
 public class ArraysPractice {
     public void arraySetup(){
         //1
+        System.out.println("Arrays 1-4: Not printed to console");
         double[] winners = new double[18];
 
-        //2
+        //2: an array of prime numbers
         int[] first_array = {2, 3, 5, 7, 11, 13, 17, 19};
 
         //3
@@ -16,6 +17,7 @@ public class ArraysPractice {
         int[][] maze = new int[30][40];
 
         //5
+        System.out.println("Array 5: Really big array");
         int[][] newMaze = new int[16][32];
         for (int i=0;i<16;i++){
             for(int j=0;j<32;j++){
@@ -26,6 +28,7 @@ public class ArraysPractice {
         }
 
         //6
+        System.out.println("\nArray 6: 2d Array");
         int array6[][]= {{2,3,1},{2,2,1},{0,2,3},{1,0,4}};
         for(int i=0;i<4;i++){
             //visualizes the array line by line
@@ -35,6 +38,7 @@ public class ArraysPractice {
             System.out.println();}
 
         //7
+        System.out.println("\nArray 7: Print out final number");
         int array7[][]= {{2,2,2},{2,2,0}};
         for(int i=0;i<2;i++){
             for(int j=0;j<3;j++){
@@ -46,6 +50,7 @@ public class ArraysPractice {
         }System.out.println();
 
         //8
+        System.out.println("\nArray 8: Double array values");
         int array8[][]= {{6,4,3},{4,3,2}};
         for(int i=0;i<2;i++){
             for(int j=0;j<3;j++){
--- src/ArraysTest.java
+++ src/ArraysTest.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class ArraysTest {
     public static void main(String[] args) {
-        ArraysPractice aP = new ArraysPractice();
-        aP.arraySetup();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to showcase the arrays? \'Y\' or \'N\'");
+        String c = sc.next();
+        if (c.toLowerCase().equals("y")){
+            ArraysPractice aP = new ArraysPractice();
+            aP.arraySetup();
+        } else if (c.toLowerCase().equals("n")){
+            System.out.println("Okay, goodbye");
+        } else {
+            System.out.println("Invalid answer");
+        }
     }
 }
